36960b1df8096366c33c0f048eaee231a8ff4ebe
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-30 21:07:38 +0400

    Создан новый проект Calculator.java

--- src/main/java/com/java24hours/Calculator.java
+++ src/main/java/com/java24hours/Calculator.java
@@ -1,2 +1,11 @@
-package com.java24hours;public class Calculator {
+package com.java24hours;
+
+public class Calculator {
+    public static void  main(String[] arguments) {
+        float sum = 0;
+        for (String argument : arguments) {
+            sum = sum + Float.parseFloat(argument);
+        }
+        System.out.println("Сумма этих чисел равна " + sum);
+    }
 }
